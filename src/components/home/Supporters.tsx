d91a51e5535baa3a7995ab3b36661487a752fb4d
Mohamed Nabil Ouf <104600074+Mohamedouf125@users.noreply.github.com>
2025-05-13 17:29:23 +0300

    Add translations for supporters metrics in English; update Supporters component to utilize new translation keys for improved localization and clarity.

--- src/components/home/Supporters.tsx
+++ src/components/home/Supporters.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Image from 'next/image';
 import partnersImg from '../../../public/images/home/Our Partners.png';
 import bgImage from '../../../public/images/home/525ab7523c86871fbf6680382ffeb83b63451acc.jpg';
+import { useTranslations } from 'next-intl';
 
 const SupportersPage: React.FC = () => {
+    const t = useTranslations('supporters');
     return (
         <section className="text-white ">
             {/* Metrics with background image and semi-transparent overlay */}
@@ -15,27 +17,27 @@ const SupportersPage: React.FC = () => {
                 <div className="absolute inset-0 bg-[#060B0EE0]" />
                 <div className="relative md:border-r md:border-white/50">
                     <span className="text-4xl font-bold">
-                        25<span className="text-2xl align-top">+</span>
+                        20<span className="text-2xl align-top">+</span>
                     </span>
-                    <p className="mt-2 text-sm">Total Years Of Experience</p>
+                    <p className="mt-2 text-sm">{t("years")}</p>
                 </div>
                 <div className="relative md:border-r md:border-white/50">
                     <span className="text-4xl font-bold">
                         2.5K<span className="text-2xl align-top">+</span>
                     </span>
-                    <p className="mt-2 text-sm">Units Sold</p>
+                    <p className="mt-2 text-sm">{t('units')}</p>
                 </div>
                 <div className="relative md:border-r md:border-white/50">
                     <span className="text-4xl font-bold">
                         170<span className="text-2xl align-top">+</span>
                     </span>
-                    <p className="mt-2 text-sm">Total Constructions</p>
+                    <p className="mt-2 text-sm">{t("constructions")}</p>
                 </div>
                 <div className="relative">
                     <span className="text-4xl font-bold">
                         2K<span className="text-2xl align-top">+</span>
                     </span>
-                    <p className="mt-2 text-sm">Customers Number</p>
+                    <p className="mt-2 text-sm">{t("clients")}</p>
                 </div>
             </div>
 
