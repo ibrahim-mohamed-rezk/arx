889e85b8c2e5c3ff53f71f0e92cae5364f6a7c13
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-05-17 16:55:08 +0300

    Update ProjectType interface in types.ts to enhance property structure by adding missing fields and improving type definitions for better clarity and maintainability.

--- src/libs/types/types.ts
+++ src/libs/types/types.ts
@@ -1,58 +1,61 @@
-export interface ProjectType{
+export interface ProjectType {
+  id: number;
+  propert_listing_no: string | null;
+  image: string;
+  cover: string;
+  icon: string;
+  home_area: string;
+  year_built: string;
+  brochure: string;
+  start_price: string;
+  end_price: string;
+  title: string;
+  description: string;
+  keywords: string;
+  slug: string;
+  meta_title: string;
+  meta_description: string;
+  meta_keywords: string;
+  catigories: Array<{
     id: number;
-    propert_listing_no: string | null;
+    name: string;
+  }>;
+  location: string;
+  features: Array<{
+    id?: number;
+    key?: string;
+    value?: string;
+  }>;
+  amenities: Array<{
+    id: number;
+    type: string;
+    title: string;
+  }>;
+  property_floor_plans: Array<{
+    id: number;
+    property_listing_id: number;
     image: string;
-    cover: string;
-    icon: string;
-    home_area: string;
-    year_built: string;
-    brochure: string;
-    start_price: string;
-    end_price: string;
     title: string;
     description: string;
-    keywords: string;
-    slug: string;
-    meta_title: string;
-    meta_description: string;
-    meta_keywords: string;
-    location: string;
-    features: Array<{
-        id?: number;
-        key?: string;
-        value?: string;
-    }>;
-    amenities: Array<{
-        id: number;
-        type: string;
-        title: string;
-    }>;
-    property_floor_plans: Array<{
-        id: number;
-        property_listing_id: number;
-        image: string;
-        title: string;
-        description: string;
-        created_at: string;
-        updated_at: string;
-        
-    }>;
-    property_listing_images: Array<{
-        id: number;
-        image: string;
-    }>;
-    property_payment_plans: Array<{
-        id: number;
-        down_payment: string;
-        monthly_payment: string;
-        years: string;
-        text: string;
-    }>;
-    property_listing_videos: Array<{
-        id: number;
-        video: string;
-        type: string;
-    }>;
+    created_at: string;
+    updated_at: string;
+  }>;
+  property_listing_images: Array<{
+    id: number;
+    image: string;
+  }>;
+  property_payment_plans: Array<{
+    id: number;
+    down_payment: string;
+    monthly_payment: string;
+    years: string;
+    text: string;
+  }>;
+  property_listing_videos: Array<{
+    id: number;
+    video: string;
+    type: string;
+  }>;
 }
 
 
