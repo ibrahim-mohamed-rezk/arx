765e78648a76f0dd85a8c375b25b031a1a9a3a11
Mohamed Nabil Ouf <104600074+Mohamedouf125@users.noreply.github.com>
2025-05-24 15:28:26 +0300

    Adjust SVGComponent height in AboutHome for improved layout consistency.

--- src/components/home/AboutHome.tsx
+++ src/components/home/AboutHome.tsx
@@ -26,7 +26,7 @@ const AboutHome: NextPage = () => {
             />
 
               <div className="absolute -bottom-6 shadow left-6 bg-[#F1F3F3] p-4 rounded-lg">
-                <SVGComponent className="h-20 w-auto mb-2" />
+                <SVGComponent className="h-15 w-auto mb-2" />
               </div>
             </div>
           </div>
